0f2c910c02c263fae80939dbc6ffada6feb3f8d2
unknown <2019117@clt.net>
2019-08-27 14:48:43 +0900

    init

---
+++ demo-STOMPoverWebsocket/src/main/java/hello/WebSocketSendToUserConfig.java
@@ -0,0 +1,60 @@
+package hello;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketSendToUserConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic/", "/queue/");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+    
+    
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new UserInterceptor());
+    }
+    
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    	   registry
+           .addEndpoint("/one")
+           .setAllowedOrigins("*")
+           .setHandshakeHandler(new DefaultHandshakeHandler() {
+
+               public boolean beforeHandshake(
+                       ServerHttpRequest request,
+                       ServerHttpResponse response,
+                       WebSocketHandler wsHandler,
+                       Map attributes) throws Exception {
+
+                   if (request instanceof ServletServerHttpRequest) {
+                       ServletServerHttpRequest servletRequest
+                               = (ServletServerHttpRequest) request;
+                       HttpSession session = servletRequest
+                               .getServletRequest().getSession();
+                       attributes.put("sessionId", session.getId());
+                   }
+                   return true;
+               }
+           }).withSockJS();
+    }
+}
